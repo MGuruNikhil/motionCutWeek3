fc2c813b132d4e61013ba05c0c91399f6a252465
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-05-26 14:05:51 +0530

    added comments

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ document.getElementById("navButton").addEventListener('click', function () {
     const src = button.getAttribute('src');
 
     if (src === "./assets/menu.png") {
-        button.setAttribute("src", "./assets/cancel.png");
+        button.setAttribute("src", "./assets/cancel.png"); //changing the icon on clicking it
         const nav = document.createElement('div');
         nav.innerHTML = `<a href="">Home</a>
         <a href="">Products</a>
         <a href="">About</a>
         <a href="">Contact Us</a>`;
         nav.setAttribute('id', 'navList');
-        document.body.appendChild(nav);
+        document.body.appendChild(nav); //appending the vertical navigation list
         setTimeout(() => {
             nav.classList.add('show');
         }, 100);
     } else {
-        button.setAttribute("src", "./assets/menu.png");
+        button.setAttribute("src", "./assets/menu.png"); //changing the icon on clicking it
         const nav = document.getElementById("navList");
         nav.classList.remove('show');
         setTimeout(() => {
